100f8925d861c90aba66e4701f2d9e729921faaf
高泳生 <fireeagle5408@gmail.com>
2016-08-11 16:28:51 +0000

    B done

--- B.php
+++ B.php
@@ -1,9 +1,35 @@
 <?php
-$number=1;
+$number;
+$numberS;
+$set=["1","2","3","4"];
+$setBackUpH=[""];
+$setBackUpT=[""];
+$hundreds;
+$tens;
+$ones;
 
-for($i=4 ; $i>=2 ; $i--){
-    $number *= $i;
+for($i=0 ; $i<=3 ; $i++){
+    $hundreds=$set[$i];
+    $setBackUpH=$set;
+    unset($setBackUpH[$i]);
+    for($j=0 ; $j<=6 ; $j++){
+        $tens=$setBackUpH[$j];
+        $setBackUpT=$setBackUpH;
+        unset($setBackUpT[$j]);
+            for($k=0 ; $k<=3 ; $k++){
+            $ones = $setBackUpT[$k];
+            if($hundreds!="" && $tens!="" && $ones!="")
+            {$number = $hundreds.$tens.$ones;
+            echo $number."<br>";
+            }
+            }
+    }
 }
-    echo $number;
 
+
+$numberS=1;
+for($i=4 ; $i>=2 ; $i--){
+    $numberS *= $i;
+}
+    echo $numberS." sets<br>";
 ?>
